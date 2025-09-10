607ae80619c031a5dbcf64898b802930d3d76ac7
Kishanandro21 <visiontechno21@gmail.com>
2025-09-10 15:36:33 +0530

    Delete themes/default directory

--- themes/default/shared/form-msg.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="alert alert-success alert-dismissible">
-  <i class="bi bi-check-circle-fill"></i>
-  {{ $msg }}
-  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-</div>
